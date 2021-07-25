2cab92d184cefb866e292540022ac45926dd84fa
syed-arsalan-arshad <syedarsalanarshad808@gmail.com>
2021-07-25 21:06:10 +0530

    Add File

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user');
-
+const rootRoutes = require('./routes/root');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(rootRoutes);
 app.use('/api', userRoutes);
 
 mongoose.connect("mongodb://arsalan:FDZEJZQbUannzEjF@cluster0-shard-00-00.akrhh.mongodb.net:27017,cluster0-shard-00-01.akrhh.mongodb.net:27017,cluster0-shard-00-02.akrhh.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-c4rbno-shard-0&authSource=admin&retryWrites=true&w=majority")
--- routes/root.js
+++ routes/root.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const userController = require('../controllers/user');
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -11,15 +10,5 @@ router.get('/', (req, res, next) => {
         userDelete_URL: "DELETE --->   https://terasol-task-arsalan.herokuapp.com/api/people/:userId"
     });
 });
-
-router.post('/people', userController.createUser);
-
-router.get('/people', userController.fetchAllUser);
-
-router.get('/people/:userId', userController.fetchSingleUser);
-
-router.put('/people/:userId', userController.updateUser);
-
-router.delete('/people/:userId', userController.deleteUser);
 
 module.exports = router;
